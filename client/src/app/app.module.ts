111c56a2a5e3c365e2d67d0f2ce717980185bbb6
Nicholas Rodman <gitnicholas@gmail.com>
2017-07-19 16:31:21 -0700

    auth and database working

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,17 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
 
 import { AppComponent } from './app.component';
+import { AuthenticationComponent } from "./auth/authentication.component";
+import { HeaderComponent } from "./header.component";
+import { routing } from "./app.routing";
+import { LogoutComponent } from "./auth/logout.component";
+import { SignupComponent } from "./auth/signup.component";
+import { SigninComponent } from "./auth/signin.component";
+import { AuthService } from "./auth/auth.service";
+import { ErrorComponent } from "./errors/error.component";
+import { ErrorService } from "./errors/error.service";
 
 
 @NgModule({
   declarations: [
-    AppComponent
+      AppComponent,
+      AuthenticationComponent,
+      HeaderComponent,
+      LogoutComponent,
+      SignupComponent,
+      SigninComponent,
+      ErrorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    routing,
+    ReactiveFormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [AuthService, ErrorService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
